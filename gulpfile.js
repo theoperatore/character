aede1b6764e89a06cd6c05fb26d51dd72820458d
Alex Petersen <theoperatore@gmail.com>
2015-06-07 02:18:15 -0400

    stylus and base shield component

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var mocha = require('gulp-mocha');
 var jshint = require('gulp-jshint');
 var react = require('gulp-react');
 var stylus = require('gulp-stylus');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
 var reactify = require('reactify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
@@ -190,8 +192,21 @@ gulp.task('mocha', function() {
 ///////////////////////////////////////////////////////////////////////////////
 gulp.task('compile-css', function() {
   return gulp.src('src/ui/**/*.styl')
+    .pipe(sourcemaps.init())
     .pipe(stylus({ use : nib(), import : ['nib']}))
-    .pipe(gulp.dest('build/css'));
+    .pipe(concat('style.css'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('build'));
+})
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Watch for changes to styl files
+//
+///////////////////////////////////////////////////////////////////////////////
+gulp.task('watch-css', ['compile-css'], function() {
+  gulp.watch(['src/ui/**/*.styl'], ['compile-css']);
 })
 
 
@@ -200,7 +215,7 @@ gulp.task('compile-css', function() {
 // Watch and build js files, but also reload a browser!
 //
 ///////////////////////////////////////////////////////////////////////////////
-gulp.task('default', ['clean', 'build-vendor', 'watch-js'], function() {
+gulp.task('default', ['clean', 'build-vendor', 'watch-js', 'watch-css'], function() {
   browsersync({
     notify: true,
     port : 8080,
@@ -209,6 +224,6 @@ gulp.task('default', ['clean', 'build-vendor', 'watch-js'], function() {
     }
   });
 
-  gulp.watch(['index.html', 'build/bundle.js'], browsersync.reload);
+  gulp.watch(['index.html', 'build/bundle.js', 'build/style.css'], browsersync.reload);
   gulp.watch(['src/ui/style/*.css'], browsersync.reload);
 })
---
+++ src/ui/components/common/Stat.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var React = require('react/addons');
+var classnames = require('classnames');
+
+module.exports = React.createClass({
+  displayName : "Stat",
+
+
+  getDefaultProps : function() {
+    return ({
+      title : 'stat',
+      subtitle : 'mod',
+      score : 0,
+      background : false,
+      trained : false
+    })
+  },
+
+
+  shouldComponentUpdate : function(nextProps) {
+    return (
+      nextProps.score !== this.props.score ||
+      nextProps.title !== this.props.title ||
+      nextProps.trained !== this.props.trained ||
+      nextProps.subtitle !== this.props.subtitle
+    );
+  },
+
+
+  render : function() {
+    var css = classnames({
+      'stat-content' : true,
+      'stat-background' : this.props.background,
+      'stat-trained' : this.props.trained
+    });
+
+
+    return (
+      <div className="stat-container">
+        <div className="stat-title">
+          <p>{this.props.title}</p>
+        </div>
+        <div className={css}>
+          <p>{this.props.score}</p>
+        </div>
+        <div className="stat-subtitle">
+          <p>{this.props.subtitle}</p>
+        </div>
+      </div>
+    )
+  }
+})
