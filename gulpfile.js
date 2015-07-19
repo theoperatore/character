379d0bda72d4fff4cf21418887f5a02c638b7f3c
Alex Petersen <theoperatore@gmail.com>
2015-07-19 00:17:03 -0400

    gulpfile typo fix

--- gulpfile.js
+++ gulpfile.js
@@ -261,7 +261,7 @@ gulp.task('default', ['copy', 'build-vendor', 'watch-js', 'watch-css'], function
 // Just watch, don't compile vendor
 //
 ///////////////////////////////////////////////////////////////////////////////
-gulp.task('watch', ['compile-js', 'comiple-css'],  function() {
+gulp.task('watch', ['compile-js', 'compile-css'],  function() {
   browsersync({
     notify: true,
     port : 8080,
