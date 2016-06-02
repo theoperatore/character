608347d519936859a36febc9cdfe1995209da256
theoperatore <theoperatore@gmail.com>
2016-06-02 16:51:43 -0400

    rearrange ability scores

--- src/ui/panes/Abilities.js
+++ src/ui/panes/Abilities.js
@@ -108,43 +108,43 @@ export default React.createClass({
             <h5 className='info-section-title'>Abilities</h5>
           </div>
           <div className='row text-center'>
-            <div className='col-1-4 ability-stat-container'>
+            <div className='col-1-6 ability-stat-container'>
               <h6 className='ability-stat-title underline-str'>str</h6>
               <p className='ability-stat-mod'>{this.props.abilities.get('str').get('mod')}</p>
               <h6 className='ability-stat-score'>{this.props.abilities.get('str').get('score')}</h6>
             </div>
-            <div className='col-1-4 ability-stat-container'>
+            <div className='col-1-6 ability-stat-container'>
               <h6 className='ability-stat-title underline-dex'>dex</h6>
               <p className='ability-stat-mod'>{this.props.abilities.get('dex').get('mod')}</p>
               <h6 className='ability-stat-score'>{this.props.abilities.get('dex').get('score')}</h6>
             </div>
-            <div className='col-1-4 ability-stat-container separate-right'>
+            <div className='col-1-6 ability-stat-container'>
               <h6 className='ability-stat-title underline-con'>con</h6>
               <p className='ability-stat-mod'>{this.props.abilities.get('con').get('mod')}</p>
               <h6 className='ability-stat-score'>{this.props.abilities.get('con').get('score')}</h6>
             </div>
-            <div className='col-1-4 ability-stat-container'>
-              <h6 className='ability-stat-title underline-proficient'>proficiency bonus</h6>
-              <p className='ability-stat-mod'>{this.props.proficiencyBonus.get('score')}</p>
-            </div>
-          </div>
-          <div className='row text-center'>
-            <div className='col-1-4 ability-stat-container'>
+            <div className='col-1-6 ability-stat-container'>
               <h6 className='ability-stat-title underline-wis'>wis</h6>
               <p className='ability-stat-mod'>{this.props.abilities.get('wis').get('mod')}</p>
               <h6 className='ability-stat-score'>{this.props.abilities.get('wis').get('score')}</h6>
             </div>
-            <div className='col-1-4 ability-stat-container'>
+            <div className='col-1-6 ability-stat-container'>
               <h6 className='ability-stat-title underline-int'>int</h6>
               <p className='ability-stat-mod'>{this.props.abilities.get('int').get('mod')}</p>
               <h6 className='ability-stat-score'>{this.props.abilities.get('int').get('score')}</h6>
             </div>
-            <div className='col-1-4 ability-stat-container separate-right'>
+            <div className='col-1-6 ability-stat-container'>
               <h6 className='ability-stat-title underline-cha'>cha</h6>
               <p className='ability-stat-mod'>{this.props.abilities.get('cha').get('mod')}</p>
               <h6 className='ability-stat-score'>{this.props.abilities.get('cha').get('score')}</h6>
             </div>
-            <div className={`col-1-4 ability-stat-container ${perceptionTrained ? 'proficient' : ''}`}>
+          </div>
+          <div className='row text-center mt4'>
+            <div className='col-1-2 ability-stat-container'>
+              <h6 className='ability-stat-title underline-proficient'>proficiency bonus</h6>
+              <p className='ability-stat-mod'>{this.props.proficiencyBonus.get('score')}</p>
+            </div>
+            <div className={`col-1-2 ability-stat-container ${perceptionTrained ? 'proficient' : ''}`}>
               <h6 className='ability-stat-title'>passive perception</h6>
               <p className='ability-stat-mod'>{this.props.passivePerception.get('score')}</p>
             </div>
