0995865b652e9f22e1a05537e37ce71875e0d7aa
theoperatore <theoperatore@gmail.com>
2016-06-19 13:44:12 -0400

    updated landing page

--- src/ui/Landing/Landing.js
+++ src/ui/Landing/Landing.js
@@ -1,9 +1,8 @@
 'use strict';
 
 import React from 'react';
-import { db, ref } from '../../api';
 import { signInWithEmail, signOut } from '../state/actions';
-import { ROUTE_PROFILE } from '../routes';
+import { ROUTE_LOGIN } from '../routes';
 import Router from '../router/Router';
 
 export default React.createClass({
@@ -11,10 +10,6 @@ export default React.createClass({
 
   loginTestUser() {
     // this.props.dispatch(signInWithEmail('test@test.com', 'pcTest'));
-  },
-
-  navigateToProfile() {
-    Router.nav(ROUTE_PROFILE);
   },
 
   componentDidMount() {
@@ -27,28 +22,11 @@ export default React.createClass({
       "padding" : 5
     }
 
-    let isSignedIn = this.props.state.status.get('userSignedIn');
-    let isAuthenticating = this.props.state.status.get('userAuthenticating');
-    let authErrorMsg = this.props.state.status.getIn(['userAuthenticationError', 'message']);
-
     return (
       <div style={style}>
         <h1>Pocket Character</h1>
         <hr />
-        {
-          isSignedIn
-          ? <button
-              onClick={this.navigateToProfile}
-            >Go to Profile</button>
-          : <button 
-              onClick={this.loginTestUser}
-              disabled={isAuthenticating}
-            >{isAuthenticating ? 'Signing In...' : 'Test Authenticate'}</button>
-        }
-        <hr />
-        <p style={{ display: 'none' }} disabled={true} className='link'>Sign In</p>
-        <hr />
-        <p className='text-red'>{authErrorMsg}</p>
+        <p className='link' onClick={() => Router.nav(ROUTE_LOGIN)}>Sign In</p>
       </div>
     )
   }
