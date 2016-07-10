19bc32854d8cb6a5121d509c001222ff968e202c
theoperatore <theoperatore@gmail.com>
2016-07-10 19:28:49 -0400

    notifications and lib updates

---
+++ src/ui/components/Notification/index.js
@@ -0,0 +1,94 @@
+'use strict';
+
+import React, { PropTypes } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import cn from 'classnames';
+import Icon from '../Icon';
+
+export default React.createClass({
+  displayName: 'Notification',
+
+  propTypes: {
+    id: PropTypes.string.isRequired,
+    active: PropTypes.bool.isRequired,
+    onDismiss: PropTypes.func.isRequired,
+    message: PropTypes.string,
+    component: PropTypes.element,
+    direction: PropTypes.string,
+    onClick: PropTypes.func,
+  },
+
+  getDefaultProps() {
+    return {
+      direction: 'left',
+      onClick() {},
+      message: 'Notification',
+    }
+  },
+
+  dismissTimeout: null,
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.active === true) {
+      this.dismissTimeout = setTimeout(() => {
+        this.props.onDismiss();
+
+      // hardcoding until a better way of keeping the enter/exit animation
+      // in sync with this duration is found.
+      }, 4000);
+    }
+    else {
+      clearTimeout(this.dismissTimeout);
+    }
+  },
+
+  componentWillUnmount() {
+    clearTimeout(this.dismissTimeout);
+  },
+
+  _click(ev) {
+    ev.preventDefault();
+    ev.stopPropagation();
+
+    this.props.onClick(ev);
+  },
+
+  renderMessage() {
+    return <div className='pr2 text-blue bg-blue timer'>
+      <p><Icon icon='fa fa-exclamation' className='p2'/>{this.props.message}</p>
+    </div>
+  },
+
+  render() {
+    let notifCSS = cn({
+      'notification': true,
+      'from-left': this.props.direction !== 'right',
+      'from-right': this.props.direction === 'right',
+    });
+
+    return <ReactCSSTransitionGroup
+      component={OnlyChild}
+      transitionEnterTimeout={300}
+      transitionLeaveTimeout={300}
+      transitionName='notification'
+    >
+      {
+        this.props.active
+          ?
+              <div
+                className={notifCSS}
+                onClick={this._click}
+              >
+                { this.props.component || this.renderMessage() }
+              </div>
+          : null
+      }
+    </ReactCSSTransitionGroup>
+  },
+})
+
+let OnlyChild = React.createClass({
+  render() {
+    return React.Children.toArray(this.props.children)[0] || null;
+  }
+})
