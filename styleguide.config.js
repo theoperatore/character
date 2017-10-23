6dd910e9dc6e8eee9ad97ec94a25e24a2990bec4
Al Petersen <al@spotify.com>
2017-10-22 21:16:41 -0400

    styleguidist config

--- styleguide.config.js
+++ styleguide.config.js
@@ -4,8 +4,21 @@ module.exports = {
   require: [
     // so that components have access to the color vars
     path.resolve(__dirname, 'src/index.css'),
+    // so that components have access to the icon fonts
     path.resolve(__dirname, 'src/hippo.css'),
     path.resolve(__dirname, 'node_modules/font-awesome/css/font-awesome.min.css'),
+  ],
+  title: 'Character (v3) Style Guide',
+  sections: [
+    {
+      name: 'Atomic Components',
+      content: 'src/components/atomic-components.md',
+      components: 'src/components/{Button,Checkbox,Icon,Input,LoadingIndicator,Modal,Select}/*.js',
+    },
+    {
+      name: 'Layout Components',
+      content: 'src/components/layout-components.md',
+    }
   ],
   theme: {
     color: {
