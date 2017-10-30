798e1c82b1465939541bd10f91cd3a02aee82a10
Al Petersen <al@spotify.com>
2017-10-29 23:53:36 -0400

    no more important

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import { createState } from './state';
 import registerServiceWorker from './registerServiceWorker';
 import registerDatabase from './registerDatabase';
-import App from './App';
 
 import 'firebaseui/dist/firebaseui.css';
 import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
 import './hippo.css';
 
+// import App after global css so that components can override
+// the styles as neccessary
+import App from './App';
 const store = createState();
 
 registerServiceWorker();
